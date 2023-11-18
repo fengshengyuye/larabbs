cb73246b3e90126e9bcc34ffcef412f80791214d
fengshengyuye <2669328295@qq.com>
2023-11-18 14:59:31 +0800

    显示个人资料

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -11,10 +11,10 @@
                 <img class="card-img-top" src="https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fsafe-img.xhscdn.com%2Fbw1%2F8ca61ee2-dbe4-41a7-93e1-893930c9a3c4%3FimageView2%2F2%2Fw%2F1080%2Fformat%2Fjpg&refer=http%3A%2F%2Fsafe-img.xhscdn.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=auto?sec=1702877282&t=10f380728f3df61620416e101209e023" alt="{{ $user->name }}">
                 <div class="card-body">
                     <h5><strong>个人简介</strong></h5>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. </p>
+                    <p>{{ $user->introduction }}</p>
                     <hr>
                     <h5><strong>注册于</strong></h5>
-                    <p>January 01 1901</p>
+                    <p>{{ $user->created_at->diffForHumans() }}</p>
                 </div>
             </div>
         </div>
